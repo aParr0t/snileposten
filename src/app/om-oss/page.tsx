c1932762ec4e7f6d1a2137a892a9eaf050a5a596
aParrot <a.lapenas04@gmail.com>
2024-02-07 19:44:19 +0100

    Update about page layout and logo

--- src/app/om-oss/page.tsx
+++ src/app/om-oss/page.tsx
@@ -1,28 +1,27 @@
 import Image from "next/image";
 
-import Logo from "/public/static/images/logo.svg";
+import Logo from "/public/static/images/om-oss.svg";
 
 export default function About() {
   return (
-    <div className="flex flex-col justify-center text-primary p-8 max-w-wideProse mx-auto">
-      <h1 className="text-4xl text-center">Om oss:</h1>
-      <div className="flex flex-col lg:flex-row gap-6 bg-secondary rounded-lg p-3 mt-4 items-center">
+    <div className="flex flex-col-reverse md:flex-row gap-6 bg-secondary-light rounded-lg my-20 items-center max-w-[90ch] mx-auto">
+      <div className="p-5">
+        <h1 className="text-primary text-5xl text-center">Om oss</h1>
         <p className="text-lg lg:text-base">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa at
-          vitae nemo rerum dicta obcaecati iste. Nobis eos sint eaque quidem
-          ullam quos sit quaerat. Illo, assumenda nam vel provident ut veniam
-          aperiam asperiores ipsa deleniti error repellat corporis nihil. Autem
-          temporibus vitae inventore consectetur magni nisi distinctio, sint
-          quisquam voluptatum maiores saepe tempore necessitatibus sapiente,
-          laudantium dignissimos expedita unde a corrupti. Iure assumenda
-          officia fugit mollitia velit neque. Cumque hic dolores repellat, rem
-          fugiat provident odio laudantium aspernatur porro pariatur earum
-          explicabo architecto suscipit perspiciatis quos quasi harum blanditiis
-          itaque? Culpa minima iure fuga aliquam placeat consequuntur ipsa
-          atque!
+          Lillestortinget ble formet av annet Sigurd Sætre i 2014. Siden den
+          gang har det foregått her på Lillestrøm VGS hvert år. I år starter vi
+          opp 13. mars og holder det gående i rundt to uker. Lillestrotinget er
+          et politisk rollespill der alle elever på vg1 ST, 2MK, og 2MD deltar.
+          Vi har også inkludert flere elever fra ulike klassetrinn, samt
+          deltakere fra politikk- og menneskerettighetsklassen. Noen av
+          programfagsklassene har gjesteroller i løpet av prosjektperioden. Vi
+          har som formål og gi økt kunnskap om demokratiet i Norge, og hvordan
+          man jobber på Stortinget. Elevene vil få innsikt i hvordan kunnskap
+          innenfor spesifikke fagområder, samt erfaringer fra ulike fagområder,
+          spiller en betydelig rolle i prosessen med å ta beslutninger.
         </p>
-        <Image src={Logo} alt="logo" width={200} height={200} />
       </div>
+      <Image src={Logo} alt="logo" width={400} height={400} />
     </div>
   );
 }
