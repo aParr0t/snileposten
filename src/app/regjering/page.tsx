05df26367384dae2cfe7e0f1ad040fe8ccdb375c
aParrot <a.lapenas04@gmail.com>
2024-02-17 13:53:08 +0100

    fixed visual bug where last list items had bottom border

--- src/app/regjering/page.tsx
+++ src/app/regjering/page.tsx
@@ -10,7 +10,7 @@ export default async function Government() {
 
   return (
     <div className="mx-auto py-4 px-4">
-      <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 relative">
         {government.map((person) => (
           <div
             key={person.id}
@@ -21,6 +21,7 @@ export default async function Government() {
               alt={person.name}
               width={200}
               height={200}
+              className="md:w-[150px] w-1/3"
             />
             <div className="flex flex-col text-secondary text-xl">
               <h3 className="text-2xl font-bold">{person.role}</h3>
@@ -29,6 +30,9 @@ export default async function Government() {
             </div>
           </div>
         ))}
+        <div className="absolute bg-white w-full h-[10px] bottom-0 scale-[1.05]">
+          {/* this div removes the borders of the last list items */}
+        </div>
       </div>
     </div>
   );
